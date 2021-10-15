4cb88a4366919c70dac5d8d1ba01a9ea9e5a8965
SangHyunGil <zizon5941@gmail.com>
2021-10-15 22:41:16 +0900

    쿼리 최적화

--- src/main/java/register/demo/domain/board/BoardH2Repository.java
+++ src/main/java/register/demo/domain/board/BoardH2Repository.java
@@ -15,16 +15,19 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class BoardH2Repository implements BoardRepository{
 
     private final EntityManager em;
 
     public Long savePost(Board board) {
+        log.info("savePost");
         em.persist(board);
         return board.getId();
     }
 
     public Boolean updatePost(Long id, BoardForm boardForm) {
+        log.info("updatePost");
         Board originBoard = em.find(Board.class, id);
         originBoard.setTitle(boardForm.getTitle());
         originBoard.setContent(boardForm.getContent());
@@ -33,6 +36,7 @@ public class BoardH2Repository implements BoardRepository{
     }
 
     public Boolean deletePost(Long id) {
+        log.info("deletePost");
         Board board = em.find(Board.class, id);
         board.setIsDelete(true);
 
@@ -40,22 +44,26 @@ public class BoardH2Repository implements BoardRepository{
     }
 
     public Board findById(Long id) {
+        log.info("BoardFindById");
         return em.find(Board.class, id);
     }
 
     public List<Board> findByTitle(String title) {
+        log.info("BoardFindByTitle");
         return em.createQuery("select b from Board b where b.title like concat('%', :title, '%')", Board.class)
                 .setParameter("title", title)
                 .getResultList();
     }
 
     public List<Board> findByStudent(Student student) {
+        log.info("BoardFindByStudent");
         return em.createQuery("select b from Board b where b.writer =: student", Board.class)
                 .setParameter("student", student)
                 .getResultList();
     }
 
     public List<Board> findAllPost() {
+        log.info("BoardFindAllPost");
         return em.createQuery("select b from Board b where b.isDelete=:isDelete", Board.class)
                 .setParameter("isDelete", false)
                 .getResultList();
